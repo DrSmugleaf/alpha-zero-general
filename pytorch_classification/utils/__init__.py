897a271a9aa969895127383cadfe5f08bcae26ad
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2019-09-16 14:41:15 +0200

    Added pytorch_classification and progress

---
+++ pytorch_classification/utils/__init__.py
@@ -0,0 +1,12 @@
+"""Useful utils
+"""
+# progress bar
+import os
+import sys
+
+from .eval import *
+from .logger import *
+from .misc import *
+from .visualize import *
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
